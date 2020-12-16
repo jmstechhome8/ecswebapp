63cb142fb36ace430ffcf865bf168af11b925549
gkaramide <45131224+gkaramide@users.noreply.github.com>
2020-12-16 02:31:27 -0800

    Add files via upload

---
+++ devops/variables.tf
@@ -0,0 +1,50 @@
+variable "aws_region" {
+  description = "The AWS region things are created in"
+  default     = "us-east-1"
+}
+
+variable "ecs_task_execution_role_name" {
+  description = "ECS task execution role name"
+  default = "myEcsTaskExecutionRole"
+}
+
+variable "ecs_auto_scale_role_name" {
+  description = "ECS auto scale role Name"
+  default = "myEcsAutoScaleRole"
+}
+
+variable "az_count" {
+  description = "Number of AZs to cover in a given region"
+  default     = "2"
+}
+
+variable "app_image" {
+  description = "Docker image to run in the ECS cluster"
+  default     = "932254444091.dkr.ecr.us-east-1.amazonaws.com/mywebapp"
+}
+
+variable "app_port" {
+  description = "Port exposed by the docker image to redirect traffic to"
+  default     = 8080
+}
+
+variable "app_count" {
+  description = "Number of docker containers to run"
+  default     = 1
+}
+
+variable "health_check_path" {
+  default = "/"
+}
+
+variable "cpu" {
+  description = "EC2 instance CPU units to provision (1 vCPU = 1024 CPU units)"
+  default     = "512"
+}
+
+variable "memory" {
+  description = "EC2 instance memory to provision (in MiB)"
+  default     = "1024"
+}
+variable "tag" {
+}
